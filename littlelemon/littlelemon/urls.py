87fec248e09c01f2782cac99dce27a4e48dbb8c0
adrinho <messib10.10@gmail.com>
2023-03-04 22:56:56 +0100

    add security authentication

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
 from rest_framework.routers import DefaultRouter
 from restaurant import views
 
@@ -24,8 +23,10 @@ router.register('tables', views.BookingViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('restaurant/', include('restaurant.urls')),
+    path('restaurant/', include('restaurant.urls')),
     path('restaurant/menu/', include('restaurant.urls')),
     path('', include(router.urls)),
     path('restaurant/booking/', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
